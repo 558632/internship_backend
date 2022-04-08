f8dfb748eb181193668c56106ab5256e07be671e
558632 <jancik20@stud.uniza.sk>
2022-04-08 16:37:16 +0200

    end_point_post_patient

--- src/api/v1/patients/post.patient.ts
+++ src/api/v1/patients/post.patient.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from 'express'
+import {Request, Response, NextFunction} from 'express'
 import Joi from 'joi'
 import { models } from '../../../db'
 import {GENDER, GENDERS} from './../../../utils/enums'
@@ -7,7 +7,7 @@ export const schema = Joi.object({
     body: Joi.object({
         firstName: Joi.string().max(100).required(),
         lastName: Joi.string().max(100).required(),
-        birthdate: Joi.date().required(),
+        birthdate: Joi.date().required().iso().less('2022-01-01'),
         weight: Joi.number().integer().min(1).max(200).required(),
         height: Joi.number().integer().min(1).required(),
         identificationNumber: Joi.string().pattern(/^[a-zA-Z0-9]*$/).length(12).required(),
@@ -29,46 +29,48 @@ interface IPatient{
     diagnoseID: number
 }
 
-export const workflow = async (req: Request, res: Response) => {
+export const workflow = async (req: Request, res: Response, next:NextFunction) => {
+    try{
+        const { Patient, Diagnose } = models
+        const {body, params, query} : {body:IPatient, params:any, query:any} = req
 
-    const { Patient, Diagnose } = models
-    const {body, params, query} : {body:IPatient, params:any, query:any} = req
-
-    const diagnose = await Diagnose.findAll({
-        where:{
-            id: body.diagnoseID
-        }
-    })
-
-    if (diagnose.length === 0){
-        res.status(404).json({
-            message: 'Diagnose not found.'
+        const diagnose = await Diagnose.findByPk(body.diagnoseID,{
+            attributes:['id']
         })
-    }
 
-    const patientID = await Patient.findAll({
-        where: {
-            identificationNumber: body.identificationNumber
+        if (!diagnose){
+            res.status(404).json({
+                message: 'Diagnose not found.'
+            })
         }
-    })
 
-    if (patientID.length !== 0){
-        res.status(409).json({
-            message: 'Patient with such a identificationNumber specified is already present in database.'
+        const sameIdNum = await Patient.findOne({
+            attributes:['id'],
+            where: {
+                identificationNumber: body.identificationNumber
+            }
         })
-    }
 
-    const patient = await Patient.create({
-        ...body
-    })
-
-    res.status(200).json({
-        "messages":[{
-            "message": "Patient added to database successfully",
-            "type": "SUCCESS"
-        }],
-        "patient": {
-            "id": patient.id
+        if (sameIdNum){
+            res.status(409).json({
+                message: 'Patient with such a identificationNumber specified is already present in database.'
+            })
         }
-    })
+
+        const patient = await Patient.create({
+            ...body
+        })
+
+        return res.status(200).json({
+            "messages":[{
+                "message": "Patient added to database successfully",
+                "type": "SUCCESS"
+            }],
+            patient: {
+                id: patient.id
+            }
+        })
+    }catch(error){
+        next(error)
+    }
 }
